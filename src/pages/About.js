ff0d7cbfb95f2023774101bd4da97a7570522cda
Christopher Sesugh <christohybrid185@gmail.com>
2021-06-26 20:36:27 +0100

    cocktail DB

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        tempora aut fugiat, explicabo veniam libero dolores? Nemo adipisci
+        illum, beatae corporis quibusdam suscipit quas cum velit dolor eveniet
+        saepe porro soluta ad laboriosam voluptatum praesentium ut atque quod
+        sint quasi!
+      </p>
+    </section>
+  );
+};
+
+export default About;
